678a124393ced5cd22d0e094b3c4fe1407c74d85
Marc-Gael ZEME <gaelzeme1@gmail.com>
2025-02-11 18:48:37 +0000

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Sélectionner le bouton et le paragraphe où afficher le message
-const button = document.getElementById('messageButton');
-const messageElement = document.getElementById('message');
-
-// Fonction appelée lorsqu'on clique sur le bouton
-button.addEventListener('click', function() {
-    messageElement.textContent = 'Merci d\'avoir cliqué ! 😊';
-});
